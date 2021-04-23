01c396b6add89fcdb24e11a3f35411047679dbbe
Letícia Lima <lodldesigner@gmail.com>
2021-04-22 22:38:56 -0300

    Página Empreendimentos, página contato em andamento

---
+++ app/empreendimentos.php
@@ -0,0 +1,151 @@
+<?php include_once('inc/_header.php'); ?>
+<div class="main p-empreendimentos">
+    <div class="banner-topo-page" style="background-image: url(./dist/images/bg-banner-topo.jpg);">
+		<div class="content-center">
+			<div class="text">
+				<h3 class="subtitle-box">Nossos</h3>
+				<h2 class="title-box">Empreendimentos</h2>
+			</div>
+		</div>
+    </div>
+    <div class="box-text bg-balls">
+        <div class="content-center">
+            <div class="box-figures">
+                <div class="flex-figures">
+                    <figure>
+                        <img src="./dist/images/logo1.png" alt="">
+                        <span class="icon-more"></span>
+                    </figure>
+                    <figure>
+                        <img src="./dist/images/logo1.png" alt="">
+                        <span class="icon-more"></span>
+                    </figure>
+
+                </div>
+            </div>
+            <h4>
+                A nossa parceria com a <a href="#">MaxPlural</a> garante: aqui você vai encontrar o empreendimento ideal para investir e perfeito para morar! 
+            </h4>
+        </div>
+    </div>
+    <div class="box-cards-info">
+        <div class="content-center">
+            <div class="center">
+                <div class="flex-cards">
+                    <div class="item-card">
+                        <figure>
+                            <img src="https://via.placeholder.com/388x388" alt="">
+                        </figure>
+                        <div class="info">
+                            <h3>Cupe Suítes Cupe </h3>
+                            <div class="list-info">
+                                <div class="item-info">
+                                    <img src="./dist/images/icon-local.svg" alt="">
+                                    <p>PE</p>
+                                </div>
+                                <div class="item-info">
+                                    <img src="./dist/images/icon-size.svg" alt="">
+                                    <p>26M² até 186M²</p>
+                                </div>
+                                <div class="item-info">
+                                    <img src="./dist/images/icon-house.svg" alt="">
+                                    <p>02 a 03 quartos</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-card">
+                        <figure>
+                            <img src="https://via.placeholder.com/388x388" alt="">
+                        </figure>
+                        <div class="info">
+                            <h3>Cupe Suítes Cupe </h3>
+                            <div class="list-info">
+                                <div class="item-info">
+                                    <img src="./dist/images/icon-local.svg" alt="">
+                                    <p>PE</p>
+                                </div>
+                                <div class="item-info">
+                                    <img src="./dist/images/icon-size.svg" alt="">
+                                    <p>26M² até 186M²</p>
+                                </div>
+                                <div class="item-info">
+                                    <img src="./dist/images/icon-house.svg" alt="">
+                                    <p>02 a 03 quartos</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-card">
+                        <figure>
+                            <img src="https://via.placeholder.com/388x388" alt="">
+                        </figure>
+                        <div class="info">
+                            <h3>Cupe Suítes Cupe </h3>
+                            <div class="list-info">
+                                <div class="item-info">
+                                    <img src="./dist/images/icon-local.svg" alt="">
+                                    <p>PE</p>
+                                </div>
+                                <div class="item-info">
+                                    <img src="./dist/images/icon-size.svg" alt="">
+                                    <p>26M² até 186M²</p>
+                                </div>
+                                <div class="item-info">
+                                    <img src="./dist/images/icon-house.svg" alt="">
+                                    <p>02 a 03 quartos</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-card">
+                        <figure>
+                            <img src="https://via.placeholder.com/388x388" alt="">
+                        </figure>
+                        <div class="info">
+                            <h3>Cupe Suítes Cupe </h3>
+                            <div class="list-info">
+                                <div class="item-info">
+                                    <img src="./dist/images/icon-local.svg" alt="">
+                                    <p>PE</p>
+                                </div>
+                                <div class="item-info">
+                                    <img src="./dist/images/icon-size.svg" alt="">
+                                    <p>26M² até 186M²</p>
+                                </div>
+                                <div class="item-info">
+                                    <img src="./dist/images/icon-house.svg" alt="">
+                                    <p>02 a 03 quartos</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-card">
+                        <figure>
+                            <img src="https://via.placeholder.com/388x388" alt="">
+                        </figure>
+                        <div class="info">
+                            <h3>Cupe Suítes Cupe </h3>
+                            <div class="list-info">
+                                <div class="item-info">
+                                    <img src="./dist/images/icon-local.svg" alt="">
+                                    <p>PE</p>
+                                </div>
+                                <div class="item-info">
+                                    <img src="./dist/images/icon-size.svg" alt="">
+                                    <p>26M² até 186M²</p>
+                                </div>
+                                <div class="item-info">
+                                    <img src="./dist/images/icon-house.svg" alt="">
+                                    <p>02 a 03 quartos</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include_once('inc/_footer.php'); ?>
---
+++ app/fale-conosco.php
@@ -0,0 +1,47 @@
+<?php include_once('inc/_header.php'); ?>
+<div class="main p-fale-conosco">
+    <div class="banner-topo-page" style="background-image: url(./dist/images/bg-banner-topo.jpg);">
+		<div class="content-center">
+			<div class="text">
+				<h3 class="subtitle-box">Fale conosco:</h3>
+				<h2 class="title-box">Contato</h2>
+			</div>
+		</div>
+    </div>
+    <div class="box-contact-full">
+        <div class="part-form">
+            <div class="content-center">
+                <div class="flex">
+                    <div class="side" style="background:#fff;">
+                        <h3 class="small-title">Email e Telefones</h3>
+                        <div class="list-contatos">
+                            <div class="item-contato">
+                                <a href="#">comercial@hbr.eng.br</a>
+                            </div>
+                            <div class="item-contato">
+                                <a href="#">(81) 3236.5308</a>
+                                <a href="#">(81) 3229.1788</a>
+                            </div>
+                        </div>
+                        <h2 class="title-box">Formulário de Contato</h2>
+                        <h3 class="subtitle-box">preencha os campos e entraremos em contato.</h3>
+                    </div>
+                    <div class="side" style="background:#161616;">
+            
+                    </div>
+                </div>
+        
+            </div>
+        </div>
+        <div class="part-localizacao">
+            <div class="content-center">
+                <div class="side">
+                    <h2 class="title-box">Localização</h2>
+                    <h3 class="subtitle-box">Lorem ipsum dolor sit amet, <br> consectetur adipiscing elit.</h3>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<?php include_once('inc/_footer.php'); ?>
