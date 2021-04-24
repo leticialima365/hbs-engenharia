1a63497d3e20bce4fc55b82b0c8bf156383f6527
Letícia Lima <lodldesigner@gmail.com>
2021-04-23 22:01:39 -0300

    pagina contato

--- app/fale-conosco.php
+++ app/fale-conosco.php
@@ -12,7 +12,7 @@
         <div class="part-form">
             <div class="content-center">
                 <div class="flex">
-                    <div class="side" style="background:#fff;">
+                    <div class="side">
                         <h3 class="small-title">Email e Telefones</h3>
                         <div class="list-contatos">
                             <div class="item-contato">
@@ -26,8 +26,17 @@
                         <h2 class="title-box">Formulário de Contato</h2>
                         <h3 class="subtitle-box">preencha os campos e entraremos em contato.</h3>
                     </div>
-                    <div class="side" style="background:#161616;">
-            
+                    <div class="side">
+                        <form action="">
+                            <input type="text" name="Nome" placeholder="Nome">
+                            <input type="email" name="Email" placeholder="Email">
+                            <input type="text" name="Telefone" placeholder="Telefone">
+                            <div class="textarea-space">
+                                <label for="textarea">Mensagem</label>
+                                <textarea name="mensagem"></textarea>
+                            </div>
+                            <button class="c-button" type="submite">Enviar</button>
+                        </form>
                     </div>
                 </div>
         
@@ -38,6 +47,13 @@
                 <div class="side">
                     <h2 class="title-box">Localização</h2>
                     <h3 class="subtitle-box">Lorem ipsum dolor sit amet, <br> consectetur adipiscing elit.</h3>
+                    <div class="text">
+                        <h3>Rua Oscar Berardo, Nº 42. Prado</h3>
+                        <p>Recife-PE • CEP:50751-020</p>
+                    </div>
+                </div>
+                <div class="mapa">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1602.1857334622111!2d-34.919921642145106!3d-8.025220746038732!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab19bacdbadec7%3A0xe23e0e30af4d2de3!2sCentrex%20Proj!5e0!3m2!1spt-BR!2sbr!4v1619223775952!5m2!1spt-BR!2sbr" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                 </div>
 
             </div>
